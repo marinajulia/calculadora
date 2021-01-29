7d4f06827e07ef8cbf719dd62e040471544794c8
Marina Julia <marinajulia707@gmail.com>
2021-01-29 10:25:48 -0300

    alteracoes

--- Desktop/calculadora/main.js
+++ Desktop/calculadora/main.js
@@ -1,27 +1,32 @@
+//valorTotal = (1-(Math.pow(1+porcentagemJuros),-meses)*prestacao)/porcentagemJuros
+
+
 const pegandoDadosBotao = document.querySelector('[data-form-botao]')
 
 
-
-const calcular =(evento)=>{
+const calcular = (evento) => {
+    evento.preventDefault() 
     const pegandoDadosValorTotal = document.querySelector('[data-valor-total]')
     let valorTotal = pegandoDadosValorTotal.value
     const pegandoDadosMeses = document.querySelector('[data-valor-mensal]')
     const meses = pegandoDadosMeses.value
     const pegandoDadosJuros = document.querySelector('[data-valor-juros]')
     const juros = pegandoDadosJuros.value
-    const pegandoDadosValorPrestacao = document.querySelector('[data-valor-prestacao]')
-    const prestacao = pegandoDadosValorPrestacao.value 
 
-    const porcentagemJuros = juros/100
+    const porcentagemJuros = juros / 100
 
-    //valorTotal = (1-(Math.pow(1+porcentagemJuros),meses)*prestacao)/porcentagemJuros
 
-    const quantoFicou = valorTotal*Math.pow((1 + porcentagemJuros),meses)
-    const mensal = quantoFicou/meses
+    const quantoFicou = valorTotal * Math.pow((1 + porcentagemJuros), meses)
+    const mensal = quantoFicou / meses
 
-    console.log("total que vai pagar ",quantoFicou)
-    console.log("mensal ",mensal)
-    
+
+    const tarefa = document.querySelector('[data-task]')
+    const conteudo= `<p class="content">${quantoFicou} </p>`
+    tarefa.innerHTML = conteudo
+
+    const tarefa2 = document.querySelector('[data-task2]')
+    const conteudo2= `<p class="content">${mensal}</p`
+    tarefa2.innerHTML = conteudo2
 
 
 }
