173ba701742aaf22f57072e64bcdab797ce5c04b
Marina Julia <marinajulia707@gmail.com>
2021-02-02 23:47:22 -0300

    --

---
+++ main.js
@@ -0,0 +1,37 @@
+//valorTotal = (1-(Math.pow(1+porcentagemJuros),-meses)*prestacao)/porcentagemJuros
+
+
+const pegandoDadosBotao = document.querySelector('[data-form-botao]')
+
+
+const calcular = (evento) => {
+    evento.preventDefault()
+    const pegandoDadosValorTotal = document.querySelector('[data-valor-total]')
+    let valorTotal = pegandoDadosValorTotal.value
+    const pegandoDadosMeses = document.querySelector('[data-valor-mensal]')
+    const meses = pegandoDadosMeses.value
+    const pegandoDadosJuros = document.querySelector('[data-valor-juros]')
+    const juros = pegandoDadosJuros.value
+
+    const porcentagemJuros = juros / 100
+
+
+    const quantoFicou = valorTotal * Math.pow((1 + porcentagemJuros), meses)
+    const mensal = quantoFicou / meses
+
+    const quantoFicouFinal = Math.round(quantoFicou)
+    const mensalFinal = Math.round(mensal)
+
+    const tarefa = document.querySelector('[data-task]')
+    const conteudo = `<p class="content"> R$${quantoFicouFinal},00 </p>`
+    tarefa.innerHTML = conteudo
+
+    const tarefa2 = document.querySelector('[data-task2]')
+    const conteudo2 = `<p class="content">R$${mensalFinal},00</p`
+    tarefa2.innerHTML = conteudo2
+
+
+
+}
+
+pegandoDadosBotao.addEventListener('click', calcular)
